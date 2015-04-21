19f07d0f8529c0f02811783904daecf92631e5a0
Tyler-Hilbert <tylerjoshuahilbert@gmail.com>
2015-04-21 16:48:19 -0400

    Removed recursion and  increased efficiency

--- HTMLParser/HTMLParser.cpp
+++ HTMLParser/HTMLParser.cpp
@@ -19,26 +19,28 @@ public:
 };
 
 
-void parse(string &parsed, string &line, tagsList &tags);
+void parse(string &line, tagsList &tags, int &tagIndex);
 
 
 int main(int argc, const char** argv) {
-	ifstream infile("D:\\Documents\\Computer Science\\masonacm.org.htm");
+	ifstream infile("D:\\Documents\\Computer Science\\Web\\masonacm.org.htm");
 
 	if (infile) {
 		// Read file
-		string fileData((istreambuf_iterator<char>(infile)), istreambuf_iterator<char>());
+		string in((istreambuf_iterator<char>(infile)), istreambuf_iterator<char>());
 		infile.close();
 
 		// Parse
-		string parsed = "";
 		tagsList tags;
-		parse(parsed, fileData, tags);
+		// Check for tag
+		int tagIndex = in.find_first_of('<');
+		do {
+			parse(in, tags, tagIndex);
+			tagIndex = in.find_first_of('<');
+		} while (tagIndex != -1);
 
-		// Output
-		cout << parsed;
+
 		system("pause");
-
 		return 0;
 	} else {
 		cout << "file not found!\n";
@@ -50,47 +52,39 @@ int main(int argc, const char** argv) {
 /**
  *  Parses the line string and adds it to the parsed string
  */
-void parse(string &parsed, string &in, tagsList &tags) {	
-	// Check for tag
-	int tagIndex = in.find_first_of('<');
+void parse(string &in, tagsList &tags, int &tagIndex) {	
+	// Add line up to the tag
+	string beforeTag = in.substr(0, tagIndex);
+	boost::algorithm::trim(beforeTag);
+	if (!beforeTag.empty() && tags.inWritableTag()) { 
+		cout << beforeTag + "\n";
+	}
 
-	if (tagIndex != -1) {
-		// Add line up to the tag
-		string beforeTag = in.substr(0, tagIndex);
-		boost::algorithm::trim(beforeTag);
-		if (!beforeTag.empty() && tags.inWritableTag()) {
-			parsed += beforeTag + "\n";
-		}
+	// Add important tag to stack
+	if (boost::iequals(in.substr(tagIndex, 7), "<script"))
+		tags.push("<script");
+	else if (boost::iequals(in.substr(tagIndex, 6), "<style"))
+		tags.push("<style");
+	else if (boost::iequals(in.substr(tagIndex, 5), "<body"))
+		tags.push("<body");
 
-		// Add important tag to stack
-		if (boost::iequals(in.substr(tagIndex, 7), "<script"))
-			tags.push("<script");
-		else if (boost::iequals(in.substr(tagIndex, 6), "<style"))
-			tags.push("<style");
-		else if (boost::iequals(in.substr(tagIndex, 5), "<body"))
-			tags.push("<body");
+	// Remove tag
+	in = in.substr(tagIndex);
+	int endTagIndex = in.find_first_of('>');
+	if (endTagIndex == -1) {
+		return; // TODO: make a better handling
+	}
 
-		// Remove tag
-		in = in.substr(tagIndex);
-		int endTagIndex = in.find_first_of('>');
-		if (endTagIndex == -1) {
-			return; // TODO: make a better handling
-		}
+	// Remove tag from stack
+	if (boost::iequals(in.substr(tagIndex, 8), "</script"))
+		tags.pop();
+	else if (boost::iequals(in.substr(tagIndex, 7), "</style"))
+		tags.pop();
+	else if (boost::iequals(in.substr(tagIndex, 6), "</body"))
+		tags.pop();
 
-		// Remove tag from stack
-		if (boost::iequals(in.substr(tagIndex, 8), "</script"))
-			tags.pop();
-		else if (boost::iequals(in.substr(tagIndex, 7), "</style"))
-			tags.pop();
-		else if (boost::iequals(in.substr(tagIndex, 6), "</body"))
-			tags.pop();
-
-		in = in.substr(endTagIndex + 1);
-
-
-		// Continue parsing
-		parse(parsed, in, tags);
-	} 
+	// Update in string
+	in = in.substr(endTagIndex + 1);
 }
 
 
